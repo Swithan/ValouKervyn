877d9dbf5fc24b6790eeae4fad13dd66b9a6c454
Debongnie Nathan <nathandebongnie@gmail.com>
2024-12-01 14:42:15 +0100

    Add Footer

---
+++ front/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <Header /> <!-- Always visible -->
+    <main>
+      <router-view /> <!-- Dynamic page content -->
+    </main>
+    <Footer /> <!-- Always visible -->
+  </div>
+</template> 
+
+<script>
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: {
+    Footer
+  }
+};
+</script>
+
+<style>
+@font-face {
+    font-family: 'GeosansLight';
+    src: url('@/assets/fonts/GeosansLight.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+body{
+    font-family: 'GeosansLight', sans-serif;
+    margin: 0 15%;
+    background-color: #ededed;
+}
+/* Global styles */
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+main {
+  flex: 1; /* Ensures the main content takes the remaining space */
+  padding: 1rem;
+  background-color: #ffffff;
+}
+}
+
+Footer {
+  position: sticky;
+  bottom: 0;
+}
+</style>
---
+++ front/src/components/Footer.vue
@@ -0,0 +1,68 @@
+<template>
+  <footer>
+    <hr class="footer-divider" />
+    <div>
+      <p class="contact">Contact: 0473/66.32.41 | valoukervyn@gmail.com</p>
+      <div class="social-icons">
+        <a href="https://www.facebook.com/profile.php?id=653734084" target="_blank">
+          <img src="../assets/fb.jpg" alt="Facebook">
+        </a>
+        <a href="https://www.instagram.com/valoukervyn/" target="_blank">
+          <img src="../assets/ig.jpg" alt="Instagram">
+        </a>
+      </div>
+      <p class="copyright">Copyright @ 2024 Valou Kervyn</p>
+    </div>
+  </footer>
+</template>
+  
+  <style>
+footer {
+  background-color: #ffffff;
+  color: #555555;
+  text-align: center;
+}
+
+footer .footer-divider {
+  border: none;
+  border-top: 2px solid #ededed; /* Horizontal line for separation */
+  margin: 0 auto .5rem; /* Center and add space below */
+  width: 50%; /* Keep the line shorter than the full width */
+}
+
+footer .social-icons {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 1rem;
+}
+
+footer .social-icons a {
+  display: inline-block;
+}
+
+footer .social-icons img {
+  width: 24px;
+  height: 24px;
+  transition: transform 0.3s ease, opacity 0.3s ease;
+}
+
+footer .social-icons img:hover {
+  transform: scale(1.1);
+  opacity: 0.8; /* Slightly fade on hover */
+}
+
+footer .contact {
+  margin: 0;
+  font-size: 0.9rem;
+  color: #333333;
+}
+
+footer .copyright {
+  margin-top: .3rem;
+  font-size: 0.8rem;
+  color: #333333;
+}
+
+  </style>
+  
