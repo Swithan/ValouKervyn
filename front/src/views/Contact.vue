c0e0b05130f0fb1c477f3f94416079b55d3453c0
Debongnie Nathan <nathandebongnie@gmail.com>
2024-12-05 20:44:26 +0100

    Add contact base
    Just basic copy from v1. Does not work.

--- front/src/views/Contact.vue
+++ front/src/views/Contact.vue
@@ -1,8 +1,33 @@
 <template>
-<p>Contact</p>
+    <div class="contact">
+        <div id="infos">
+            <h3 id="phone">{{contact.phone}}</h3>
+            <p>0473/66.32.41</p>
+            <h3 id="mail">E-mail </h3>
+            <p>valoukervyn@gmail.com</p>
+            <h3 id="adress">{{contact.address}}</h3>
+            <p>{{contact.city}}</p>
+        </div>
+        <router-link v-if="eventFound" to="/event" id="event">{{event.event}}</router-link>
+        <div id="error" v-else="">
+            <img src="/src/assets/ValouKervyn.jpg" id="valou" alt="Valou Kervyn">
+        </div>
+    </div>
 </template>
 
 <script>
+import { RouterLink } from "vue-router";
+import translations from "../assets/translate.json";
+import { inject, computed } from "vue";
+
+export default {
+    setup() {
+        const language = inject("language");
+        const contact = computed(() => translations[language.current].contact);
+        const event = computed(() => translations[language.current].event);
+        return { contact, event };
+    },
+};
 </script>
 
 <style>
